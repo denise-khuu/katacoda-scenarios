b50fffbc7a1f49dd44eb779265678e192be2a43c
denise-khuu <74537227+denise-khuu@users.noreply.github.com>
2021-06-01 12:12:42 +0200

    publishCourses

--- publishCourses.js
+++ publishCourses.js
@@ -5,18 +5,21 @@ const path = require('path');
 function publishCourses() {
     try{
         var files = fs.readdirSync(path.join(__dirname,'playbooks'));
-        child_process.spawnSync(`bash buildRun.sh -e katacoda`,{ shell: true, cwd: path.join(__dirname,'tutorial-compiler'), encoding: 'utf-8' });
+        let cp = child_process.spawnSync(`bash buildRun.sh -e katacoda`,{ shell: true, cwd: __dirname, encoding: 'utf-8' });
+        if(!fs.existsSync(path.join(__dirname,"tutorial-compiler","build", "output"))){
+            console.log(cp);
+        }
         files.forEach(function(file, index){
                 if(file.includes("pathway")){
                     var coursesJson = JSON.parse(fs.readFileSync(path.join(__dirname,'playbooks', file)));
                     var courses = coursesJson.courses;
-                    var srcDir = path.join(__dirname,'tutorial-compiler','build','output','katacoda');
-                    var destDir = path.join(__dirname, file.replace('-pathway.json', ''));
+                    var srcDir = path.join(__dirname,'build','output','katacoda');
+                    var destDir = path.join(__dirname, 'repo', file.replace('-pathway.json', ''));
                     if(fs.existsSync(destDir)){
                         fs.rmSync(destDir, { recursive: true })
                     }
-                    fs.mkdir(path.join(__dirname, destDir));
-                    fs.copyFile(path.join(__dirname,'tutorials', file), path.join(__dirname, file));
+                    fs.mkdir(path.join(destDir));
+                    fs.copyFile(path.join(__dirname,'tutorials', file), path.join(__dirname,'repo', file));
                     for(var i = 0; i < courses.length; i++){
                         console.log('Copy', courses[i].course_id);
                         fs.copySync(path.join(srcDir, courses[i].course_id), path.join(destDir, courses[i].course_id));
